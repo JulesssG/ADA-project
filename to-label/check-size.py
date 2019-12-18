7a27c84c9aaa140761d19a9121b2be8bd46e6677
floepfl <florian.ravasi@epfl.ch>
2019-12-18 10:36:36 +0100

    Clean repo

--- to-label/check-size.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-users = ['christina', 'flo', 'jules', 'lucien']
-
-for user in users:
-    data = pd.read_csv(f"./to-label-{user}.csv", header=None, names=["asin", "title", "description", "healthy", "vegetarian/vegan", "sport/productivity", "country"])
-    print(f"{user}: {len(data.index)}")
